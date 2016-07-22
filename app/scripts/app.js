63da94ab0db3c9f7c8962110c473beb58df58476
Pablo H. Paladino <paladinop@gmail.com>
2016-07-22 14:09:49 -0400

    mapa

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -70,8 +70,10 @@ angular
   .run(function(TabletopService,$rootScope) {
 
     $rootScope.loading = true;
-
-    TabletopService.getData().then(dataLoaded);
+    $rootScope.map = {
+      container:  d3.select("#mapa"),
+      svg: null
+    };
 
     var pymChild = new pym.Child({ polling: 1000 });
     pymChild.sendHeight();
@@ -89,5 +91,63 @@ angular
       $rootScope.data = $rootScope.fulldata[$rootScope.selectedYear].elements;
       console.log($rootScope.data);
     };
+
+    function renderMap(){
+
+        var sizes = $rootScope.map.container.node().getBoundingClientRect();
+
+        var width = sizes.width,
+            height = 600;
+
+        $rootScope.map.svg = $rootScope.map.container.append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        var chile = REGIONES_TOPOJSON; //from regiones.json
+        
+        console.log((width/100)*130);
+        var projection = d3.geoMercator()
+            .scale(650)
+            .translate([width / 2, height / 2])
+            .precision(0.1)
+//            .rotate([0,-250,0])
+            .center([-70,-39]);
+
+        var back = $rootScope.map.svg
+            .append("rect")
+            .attr("class", 'svg-back')
+            .style("fill", "#ff9900")
+            .style("opacity", 1)
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height)
+            .on("click",function(d){
+                //that.unselect();
+            });
+
+
+        var pathsGroup = $rootScope.map.svg
+            .append("g")
+            .attr("class", 'paths-group');
+
+        var regiones = pathsGroup.selectAll(".region")
+            .data(topojson.feature(chile, chile.objects.regiones).features)
+            .enter()
+            .append("path")
+            .attr("class", function(d) { return "region " + d.id; })
+            .attr("id", function(d) { return "region-" + d.id; })
+            .attr("d", d3.geoPath().projection(projection))
+            .style("fill",function(d){
+              return '#ff0000';
+              /*var value = that.getRegion(d.id);
+              return that.quantize(value.conformado_porcentaje);*/
+            });
+
+    }
+
+    //init
+    TabletopService.getData().then(dataLoaded);
+    renderMap();
     
   });
